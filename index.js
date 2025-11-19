505c88e8c8ac9d456314770dcfe097bba811af53
Xanu Xunder ✨ <xanxunder+2@yandex.com>
2025-11-19 11:55:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
 
 dotenv.config();
 
